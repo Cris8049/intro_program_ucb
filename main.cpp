ca07cfd7c8457593e47d21df4e82c73df382d3fe
Cris8049 <cristopher070404@gmail.com>
2023-03-03 10:40:14 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+    double x,y;
+    cin>> x>>y;
+    cout<< floor(x+y);
+    return 0;
+    
+
+    
+}
+
+
+
+
+
+
